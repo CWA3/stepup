46d4477b54f7f7f1d6841586ca130287d59aee92
r0259012 <r0259012@lanaken.cs.kotnet.kuleuven.be.>
2012-11-05 17:25:00 +0100

    again

--- Guestbook/src/guestbook/SignGuestbookServlet.java
+++ Guestbook/src/guestbook/SignGuestbookServlet.java
@@ -39,7 +39,7 @@ public class SignGuestbookServlet extends HttpServlet {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(greeting);
         String test = 'test';
-        String rofl ='rofl';
+        String rofl ='roddddfl';
 
 
         resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
